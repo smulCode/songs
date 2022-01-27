cb9e3367f0250eb03037129e35996400729668c3
Wesley Smulders <67417862+smulCode@users.noreply.github.com>
2022-01-27 17:35:20 +0100

    v1

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import { selectSong } from "../actions";
 import SongList from "./SongList"
+import SongDetail from "./SongDetail"
 
 const App = () => {
   return (
-  <div>
+  <div className="ui container grid">
+    <div className="ui row">
+<div className="column eight wide">
     <SongList/>
+</div>
+<div className="column eight wide">
+  <SongDetail/>
+</div>
+    </div>
   </div>);
 };
 
